307c432eb40fe51a0352304146fe22960005ce13
Reilly <reilly.webster@torqit.ca>
2022-03-20 16:29:00 -0300

    Created capacitor build

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from "@capacitor/cli";
+
+const config: CapacitorConfig = {
+  appId: "com.horrorscope.app",
+  appName: "horrorscope",
+  webDir: "dist/horrorscope",
+  bundledWebRuntime: false,
+};
+
+export default config;
