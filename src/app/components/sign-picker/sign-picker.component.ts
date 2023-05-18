d98de49075c7c5854db3bbcd5c77291886930696
mregger <eduardo.egger@torqit.ca>
2023-05-18 11:54:47 -0300

    test(date-ring) circletype

--- src/app/components/sign-picker/sign-picker.component.ts
+++ src/app/components/sign-picker/sign-picker.component.ts
@@ -1,22 +1,34 @@
-import { Component, EventEmitter, Input, Output, OnInit } from '@angular/core';
+import {
+  Component,
+  EventEmitter,
+  Input,
+  Output,
+  OnInit,
+  ElementRef,
+  ViewRef,
+  ViewChild,
+} from "@angular/core";
+import CircleType from "circletype";
 
 @Component({
-  selector: 'app-sign-picker',
-  templateUrl: './sign-picker.component.html',
-  styleUrls: ['./sign-picker.component.scss']
+  selector: "app-sign-picker",
+  templateUrl: "./sign-picker.component.html",
+  styleUrls: ["./sign-picker.component.scss"],
 })
 export class SignPickerComponent implements OnInit {
-
   @Output() public onSelection = new EventEmitter<string>();
+  // @ViewChild("signNames", null) public signRing: ElementRef;
 
-  constructor() { }
+  constructor() {}
 
   public ngOnInit(): void {
-
+    const circle = new CircleType(document.getElementById("signNames"));
+    // const circle = new CircleType(this.signRing);
+    circle.radius(200);
   }
 
   public selected(sign: string): void {
     console.dir(sign);
-    this.onSelection.emit(sign)
+    this.onSelection.emit(sign);
   }
 }
